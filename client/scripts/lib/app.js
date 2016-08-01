2740885feb7b38df0f34c8b9d7018bdbceaad7a4
IustiNN <iustin.nitza@gmail.com>
2016-08-01 15:32:33 +0300

    load routes and add tabs

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -5,9 +5,11 @@ import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
 import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
 import { Meteor } from 'meteor/meteor';
 
 //Modules
+import RoutesConfig from '../routes';
 const App = 'Whatsapp';
 
 //App
@@ -15,6 +17,9 @@ Angular.module(App, [
 	'angular-meteor',
 	'ionic'
 ]);
+
+new Loader(App)
+	.load(RoutesConfig);
 
 //Startup
 if (Meteor.isCordova) {
